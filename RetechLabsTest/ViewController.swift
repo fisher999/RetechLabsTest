fe852c0a59aeaaab11c2db2a25556e693526748c
semeeenov_viktor <vsemenov@asdgroup.pro>
2019-07-01 23:38:19 +0700

    Initial Commit

---
+++ RetechLabsTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RetechLabsTest
+//
+//  Created by Victor on 01/07/2019.
+//  Copyright © 2019 Victor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
